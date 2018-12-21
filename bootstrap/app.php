01a4fc8f6d438fb01ade0c4a1a94dae913386bd9
Gilgamesh <gilgamesh@ganguo.hk>
2018-12-21 15:45:24 +0800

    feat: some little change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,13 +3,13 @@
 $app = new \Src\App(dirname(__DIR__));
 
 $app->initializeServices([
-    \Src\Provider\RouteTableServiceProvider::class,
-    \Src\Provider\DispatchServiceProvider::class,
-    \Src\Provider\MiddlewareProvider::class,
-    \Src\Provider\HttpServiceProvider::class,
-    \Src\Provider\RequestProvider::class,
-    \Src\Provider\ResponseServerProvider::class,
-    \Src\Provider\RedisServiceProvider::class,
+    Src\Provider\RouteTableServiceProvider::class,
+    Src\Provider\DispatchServiceProvider::class,
+    Src\Provider\MiddlewareProvider::class,
+    Src\Provider\HttpServiceProvider::class,
+    Src\Provider\RequestProvider::class,
+    Src\Provider\ResponseServerProvider::class,
+    Src\Provider\RedisServiceProvider::class,
 ]);
 
 return $app;
